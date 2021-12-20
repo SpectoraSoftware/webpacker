6b1e57a25a975a8b0fec6f99eb1a6d57ce0d1251
JP Narowski <jnarowski@gmail.com>
2021-12-20 12:51:50 -0700

    Fork

--- package/rules/sass.js
+++ package/rules/sass.js
@@ -1,8 +1,12 @@
+const sass = require('saas')
 const getStyleRule = require('../utils/get_style_rule')
 
 module.exports = getStyleRule(/\.(scss|sass)(\.erb)?$/i, false, [
   {
     loader: 'sass-loader',
-    options: { sourceMap: true }
+    options: {
+      sourceMap: true,
+      implementation: sass
+    }
   }
 ])
